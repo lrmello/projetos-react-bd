caa8de02e3ebe38f8716ca2308f59d8f16da6660
lrmello <leonardo.reis28051995@gmail.com>
2023-03-16 20:54:58 -0300

    Commitando teste react-bootstrap

---
+++ react/integrar-o-react-com-bootstrap/react-bootstrap/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css' 
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Qualquer coisa</h1>
+  
+    </div>
+  )
+}
+
+export default App
---
+++ react/integrar-o-react-com-bootstrap/react-bootstrap/src/index.js
@@ -0,0 +1,4 @@
+import {createRoot} from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App'
+createRoot(document.querySelector("#root")).render(<App />)
